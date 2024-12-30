4498b76b70a580b4621e525b5311400b9f56cc5c
muskan7898 <muskannareliya7@gmail.com>
2024-12-30 16:39:16 +0530

    Added script file

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Function to greet the user
+function greetUser(name) {
+  return `Hello, ${name}! Welcome to your JavaScript practice.`;
+}
+
+// Example usage
+const userName = 'Muska';
+console.log(greetUser(userName));
